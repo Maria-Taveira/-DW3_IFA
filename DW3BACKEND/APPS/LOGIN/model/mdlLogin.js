bb36f883388b270b020ef668b19a349e5575bc65
Maria Beatriz <mbeatriz.taveira@hotmail.com>
2025-12-09 09:52:53 -0300

    atualizações

--- DW3BACKEND/APPS/LOGIN/model/mdlLogin.js
+++ DW3BACKEND/APPS/LOGIN/model/mdlLogin.js
@@ -1,15 +1,15 @@
-const db = require("../../../database/databaseconfig");
+const db = require("../../../database/databaseConfig");
 
-exports.GetCredencial = async (UserName) => {
-    const sql = `
-        SELECT userid, username, password 
-        FROM usuarios
-        WHERE username = $1 AND is_active = TRUE;
-    `;
-    try {
-        const result = await db.query(sql, [UserName]);
-        return result.rows;
-    } catch (error) {
-        throw error;
-    }
+const GetCredencial = async (loginPar) => { //busca o usuário no banco
+  return (
+    await db.query(
+      "select username, password " +
+        "from usuarios where username = $1 and deleted = false",
+      [loginPar]
+    )
+  ).rows;
+};
+
+module.exports = {
+  GetCredencial,
 };
