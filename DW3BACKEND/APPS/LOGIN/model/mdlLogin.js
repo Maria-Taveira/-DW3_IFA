d5102ba5b37102b9921b4f9488a592cc5bc5b0f4
Maria Beatriz <mbeatriz.taveira@hotmail.com>
2025-12-09 09:09:55 -0300

    Criação do login

---
+++ DW3BACKEND/APPS/LOGIN/model/mdlLogin.js
@@ -0,0 +1,15 @@
+const db = require("../../../database/databaseconfig");
+
+const GetCredencial = async (loginPar) => { //busca o usuário no banco
+  return (
+    await db.query(
+      "select username, password " +
+        "from usuarios where username = $1 and deleted = false",
+      [loginPar]
+    )
+  ).rows;
+};
+
+module.exports = {
+  GetCredencial,
+};
